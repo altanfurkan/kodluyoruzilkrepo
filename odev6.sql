c2413d22e7e38766e1532cd983e18e034551caed
Muhammet Furkan ALTAN <85619383+mfaltan1@users.noreply.github.com>
2022-01-01 15:53:24 +0300

    Create odev6.sql

---
+++ odev6.sql
@@ -0,0 +1,12 @@
+ select avg(rental_rate) from film;
+ -----------------------------------
+ select count(*) from film  
+ where title like 'C%';
+ -----------------------------------
+ select length from film
+ where rental_rate = 0.99 
+ order by length desc
+ limit 1;
+ -----------------------------------
+ select distinct count(replacement_cost) from film
+ where length > 150;
