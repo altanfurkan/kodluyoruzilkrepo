263327168ae8b4fb2cb8119bdd3a70eaee97fe4f
Muhammet Furkan ALTAN <85619383+altanfurkan@users.noreply.github.com>
2022-01-03 13:12:14 +0300

    Create odev8.sql

---
+++ odev8.sql
@@ -0,0 +1,34 @@
+update employee 
+	set birthday = '2000-01-02'	
+where id = 10;	
+
+update employee 
+	set name = 'Ali'	
+where id = 30;	
+
+update employee 
+	set name = 'Mehmet'	
+where id = 23;	
+
+update employee 
+	set name = 'Nur'	
+where id = 41;	
+
+update employee 
+	set name = 'Sevim'	
+where id = 18;	
+----------------------------------------
+delete from employee
+	where id = 12;
+	
+delete from employee
+	where id = 22;
+
+delete from employee
+	where id = 31;
+
+delete from employee
+	where id = 36;
+
+delete from employee
+	where id = 41;
